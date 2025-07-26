0482826e2e0de6b1445a9246536507066096b8c7
nikhilr-r <rajputnikhil1906@gmail.com>
2025-07-27 01:13:48 +0530

    1-updated The Landing Page 2- Adding Login Page With Auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,10 @@ import { Navbar } from "~/components/Navbar";
 
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
-import ResumeCard from "~/components/resumeCards";
+import ResumeCard from "~/components/ResumeCards";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,17 +15,24 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+   const { auth } = usePuterStore();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+         navigate('/auth?next=/');
+        }
+    },[auth.isAuthenticated]);
   return (
     <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
 
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings </h1>
           <h2>Review Your Application & Check AI-Powered Feedback</h2>
             
         </div>
-      </section>
+     
 
     {resumes.length >0 && ( 
        <div className="resumes-section"> 
@@ -31,6 +41,7 @@ export default function Home() {
       ))}
       </div>
     )}
+     </section>
     </main>
   );
 }
--- constants/index.ts
+++ constants/index.ts
@@ -4,7 +4,7 @@ export const resumes: Resume[] = [
       id: "1",
       companyName: "Google",
       jobTitle: "Frontend Developer",
-      imagePath: "/images/resume-1.png",
+      imagePath: "/images/resume_01.png",
       resumePath: "/resumes/resume-1.pdf",
       feedback: {
         overallScore: 85,
@@ -34,8 +34,8 @@ export const resumes: Resume[] = [
       id: "2",
       companyName: "Microsoft",
       jobTitle: "Cloud Engineer",
-      imagePath: "/images/resume-2.png",
-      resumePath: "/resumes/resume-2.pdf",
+      imagePath:"/images/resume_02.png",
+      resumePath: "/resumes/resume_02.pdf",
       feedback: {
         overallScore: 55,
         ATS: {
@@ -64,8 +64,98 @@ export const resumes: Resume[] = [
       id: "3",
       companyName: "Apple",
       jobTitle: "iOS Developer",
-      imagePath: "/images/resume-3.png",
-      resumePath: "/resumes/resume-3.pdf",
+      imagePath:"/images/resume_03.png",
+      resumePath: "/resumes/resume_03.pdf",
+      feedback: {
+        overallScore: 75,
+        ATS: {
+          score: 90,
+          tips: [],
+        },
+        toneAndStyle: {
+          score: 90,
+          tips: [],
+        },
+        content: {
+          score: 90,
+          tips: [],
+        },
+        structure: {
+          score: 90,
+          tips: [],
+        },
+        skills: {
+          score: 90,
+          tips: [],
+        },
+      },
+    },
+       {
+      id: "4",
+      companyName: "Google",
+      jobTitle: "Frontend Developer",
+      imagePath: "/images/resume_01.png",
+      resumePath: "/resumes/resume-1.pdf",
+      feedback: {
+        overallScore: 85,
+        ATS: {
+          score: 90,
+          tips: [],
+        },
+        toneAndStyle: {
+          score: 90,
+          tips: [],
+        },
+        content: {
+          score: 90,
+          tips: [],
+        },
+        structure: {
+          score: 90,
+          tips: [],
+        },
+        skills: {
+          score: 90,
+          tips: [],
+        },
+      },
+    },
+    {
+      id: "5",
+      companyName: "Microsoft",
+      jobTitle: "Cloud Engineer",
+      imagePath:"/images/resume_02.png",
+      resumePath: "/resumes/resume_02.pdf",
+      feedback: {
+        overallScore: 55,
+        ATS: {
+          score: 90,
+          tips: [],
+        },
+        toneAndStyle: {
+          score: 90,
+          tips: [],
+        },
+        content: {
+          score: 90,
+          tips: [],
+        },
+        structure: {
+          score: 90,
+          tips: [],
+        },
+        skills: {
+          score: 90,
+          tips: [],
+        },
+      },
+    },
+    {
+      id: "6",
+      companyName: "Apple",
+      jobTitle: "iOS Developer",
+      imagePath:"/images/resume_03.png",
+      resumePath: "/resumes/resume_03.pdf",
       feedback: {
         overallScore: 75,
         ATS: {
